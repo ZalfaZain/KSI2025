3b671809fe79743c6858731ce10998cc729c3549
ZalfaZain <zalfazain160@gmail.com>
2025-11-04 15:22:03 +0700

    Tambah KoneksiDatabase.php dan index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tambah Data</title>
+</head>
+<body>
+    <h2>Form Tambah Data</h2>
+    <form action="TambahData.php" method="post">
+        <label>Nama:</label>
+        <input type="text" name="nama" required><br><br>
+        <label>Email:</label>
+        <input type="email" name="email" required><br><br>
+        <input type="submit" value="Tambah Data">
+    </form>
+</body>
+</html>
